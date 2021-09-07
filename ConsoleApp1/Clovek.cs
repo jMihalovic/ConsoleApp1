63056fdd0f71f59c5bfe197a0644330995ff75df
mihalovic.jakub.2018 <mihalovic.jakub.2018@UC313PC13>
2021-09-07 15:17:20 +0200

    update

---
+++ ConsoleApp1/Clovek.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Clovek
+    {
+
+        public Clovek()
+        {
+
+            Jmeno = "Nezname";
+            Prijmeni = "Nezname";
+
+        }
+
+        public Clovek(string jmenoTemp, string prijmeniTemp)
+        {
+
+            Jmeno = jmenoTemp;
+            Prijmeni = prijmeniTemp;
+
+        }
+
+        public override string ToString()
+        {
+
+            return $"Jméno - {Jmeno}" +
+                $"Příjmení - {Prijmeni}";
+
+        }
+
+        public string Jmeno
+        {
+
+            get; set;
+
+        }
+
+        public string Prijmeni
+        {
+
+            get; set;
+
+        }
+    }
+}
---
+++ ConsoleApp1/Obcan.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Obcan : Clovek
+    {
+        Regex rg = new Regex(@"/[0-9]{6}\/[0-9]{4}/");
+        public Obcan()
+        {
+
+            Jmeno = "Nezname";
+            Prijmeni = "Nezname";
+            RodneCislo = "000000/0000";
+
+        }
+
+        public Obcan(Clovek cl, string rodneCis)
+        {
+
+            Jmeno = cl.Jmeno;
+            Prijmeni = cl.Prijmeni;
+            RodneCislo = rodneCis;
+
+        }
+
+        public override string ToString()
+        {
+
+            return $"Jméno - {Jmeno}" +
+                $"Příjmení - {Prijmeni}" +
+                $"Rodné číslo - {RodneCislo}";
+
+        }
+
+        public string RodneCislo
+        {
+
+            get
+            {
+
+                return RodneCislo;
+
+            }
+
+            set
+            {
+
+                if(rg.IsMatch(value))
+                {
+
+                    RodneCislo = value;
+
+                }
+
+                else
+                {
+
+                    Exception ex = new FormatException("Rodné číslo neopdpovídá formátu čččččč/čččč");
+                    throw ex;
+
+                }
+
+            }
+
+        }
+
+    }
+}
